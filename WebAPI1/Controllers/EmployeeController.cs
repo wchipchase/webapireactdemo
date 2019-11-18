6ad5e4e835744d2e02fa1c79a8116a8360435f2b
Wayne Chipchase <wayne.chipchase@gmail.com>
2019-11-17 22:23:11 -0600

    first commit

---
+++ WebAPI1/Controllers/EmployeeController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebAPI1.Models;
+
+namespace WebAPI1.Controllers
+{
+    public class EmployeeController : ApiController
+    {
+        public HttpResponseMessage Get()
+        {
+            DataTable table = new DataTable();
+
+            string query = @"select EmployeeID, EmployeeName, Department, MailID, convert(varchar(10),DOH, 120) as DOH
+                            from Employees";
+
+            using (var con = new SqlConnection(ConfigurationManager.ConnectionStrings["EmployeeAppDB"].ConnectionString))
+            using (var cmd = new SqlCommand(query, con))
+            using (var da = new SqlDataAdapter(cmd))
+            {
+                cmd.CommandType = CommandType.Text;
+                da.Fill(table);
+            }
+
+            return Request.CreateResponse(HttpStatusCode.OK, table);
+        }
+
+        public string Post(Employee emp)
+        {
+            try
+            {
+                DataTable table = new DataTable();
+
+                string doh = emp.DOH.ToString().Split(' ')[0];
+
+                string query = @"
+                 insert into dbo.Employees 
+
+                Values
+                (
+                '" + emp.EmployeeName + @"'
+                ,'" + emp.Department + @"'
+                ,'" + emp.MailID + @"'
+                ,'" + doh + @"'
+                )
+                          ";
+
+                using (var con = new SqlConnection(ConfigurationManager.ConnectionStrings["EmployeeAppDB"].ConnectionString))
+                using (var cmd = new SqlCommand(query, con))
+                using (var da = new SqlDataAdapter(cmd))
+                {
+                    cmd.CommandType = CommandType.Text;
+                    da.Fill(table);
+                }
+
+                return "Added Successfully";
+            }
+            catch (Exception ex)
+            {
+
+                return "Failed to Add";
+            }
+        }
+
+        public string Put(Employee emp)
+        {
+            try
+            {
+                DataTable table = new DataTable();
+
+                string query = @"update dbo.Employees set
+                                EmployeeName = '" + emp.EmployeeName + @"',
+                                Department = '" + emp.Department + @"',
+                                MailID = '" + emp.MailID + @"',
+                                DOH = '" + emp.DOH + @"'
+                                where EmployeeID = " + emp.EmployeeID + @"
+                                ";
+
+                using (var con = new SqlConnection(ConfigurationManager.ConnectionStrings["EmployeeAppDB"].ConnectionString))
+                using (var cmd = new SqlCommand(query, con))
+                using (var da = new SqlDataAdapter(cmd))
+                {
+                    cmd.CommandType = CommandType.Text;
+                    da.Fill(table);
+                }
+
+                return "Updated Successfully";
+            }
+            catch (Exception)
+            {
+                return "Failed to Update";
+            }
+        }
+
+        public string Delete(int id)
+        {
+            try
+            {
+                DataTable table = new DataTable();
+
+                string query = @"delete from dbo.Employees where EmployeeID =" + id;
+
+                using (var con = new SqlConnection(ConfigurationManager.ConnectionStrings["EmployeeAppDB"].ConnectionString))
+                using (var cmd = new SqlCommand(query, con))
+                using (var da = new SqlDataAdapter(cmd))
+                {
+                    cmd.CommandType = CommandType.Text;
+                    da.Fill(table);
+                }
+
+                return "Deleted Successfully";
+            }
+            catch (Exception)
+            {
+                return "Failed to Delete";
+            }
+        }
+    }
+}
+
+
+
